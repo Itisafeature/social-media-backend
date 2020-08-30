eef597bd503dabc79491101509e8c0cfffec7d7c
Itisafeature <thebigdebugger@gmail.com>
2020-08-29 21:19:12 -0400

    multer working

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
+const fs = require('fs');
 const jwt = require('jsonwebtoken');
 const { promisify } = require('util');
-
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/userModel');
 const e = require('express');
@@ -30,17 +30,39 @@ const createAndSendToken = (user, statusCode, res) => {
   });
 };
 
-exports.signup = catchAsync(async (req, res, next) => {
-  console.log(req.file.image);
-  const user = await User.create({
-    username: req.body.username,
-    email: req.body.email,
-    image: req.file.path,
-    password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm,
-  });
-  createAndSendToken(user, 201, res);
-});
+// exports.signup = catchAsync(async (req, res, next) => {
+//   const user = await User.create({
+//     username: req.body.username,
+//     email: req.body.email,
+//     password: req.body.password,
+//     passwordConfirm: req.body.passwordConfirm,
+//   });
+
+//   await User.findByIdAndUpdate(user._id, req.body);
+//   createAndSendToken(user, 201, res);
+// });
+
+exports.signup = async (req, res, next) => {
+  try {
+    const user = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      image: req.body.image,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm,
+    });
+    await fs.unlink(`public/images/${req.body.username}--0.jpg`, (err) =>
+      console.log(err)
+    );
+    createAndSendToken(user, 201, res);
+  } catch (err) {
+    await fs.unlink(`public/images/${req.body.username}--0.jpg`, (err) =>
+      console.log(err)
+    );
+    await fs.unlink(`${req.body.image}`, (err) => console.log(err));
+    next(err);
+  }
+};
 
 exports.login = async (req, res, next) => {
   const { email, password } = req.body;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const multer = require('multer');
+const sharp = require('sharp');
+const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const authController = require('../controllers/authController');
 const postRouter = require('./postRoutes');
@@ -9,7 +11,7 @@ const storage = multer.diskStorage({
     cb(null, './public/images');
   },
   filename: (req, file, cb) => {
-    cb(null, req.body.username + '-' + Date.now());
+    cb(null, req.body.username + '-' + '-0.jpg');
   },
 });
 
@@ -27,11 +29,29 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({ storage: storage, fileFilter: fileFilter });
 
+const resizeUserPhoto = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
+
+  req.body.image = `${req.body.username}-${Date.now()}.jpeg`;
+
+  await sharp(req.file.path)
+    .resize(75, 75) // Crops image to this width and height
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/images/${req.body.image}`);
+
+  req.body.image = `public/images/${req.body.image}`;
+
+  next();
+});
+
 router
   .route('/auth')
   .get(authController.protect, authController.isAuthenticated);
 
-router.route('/signup').post(upload.single('image'), authController.signup);
+router
+  .route('/signup')
+  .post(upload.single('image'), resizeUserPhoto, authController.signup);
 router.route('/login').post(authController.login);
 router.route('/logout').get(authController.logout);
 
